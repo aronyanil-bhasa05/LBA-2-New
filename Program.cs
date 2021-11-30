8d88c3aa5d2575a3b508a4854d1b71df38ab51a1
aronyanil-bhasa05 <bhasa05@gmail.com>
2021-11-30 18:53:43 +0530

    LBA 2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LabBasedAssignment2
+{
+    // Area of Circle
+    internal class Circle
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ENTER A RADIUS= ");
+            int r=Convert.ToInt32(Console.ReadLine());
+            double a = r * r * 3.14;
+            Console.WriteLine($"AREA OF CIRCLE= {a}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ asciiConversion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LabBasedAssignment2
+{
+    //Character to ASCII and Vice Versa
+    internal class asciiConversion
+    {
+        static void Main ()
+        {       
+            Console.WriteLine("Press 1 for Character to ASCII value conversion");
+            Console.WriteLine("Press 2 for ASCII value to Character conversion");
+            Console.Write("Enter Option= ");
+            int op = Convert.ToInt32(Console.ReadLine());
+            if (op==1)
+            {
+                Console.Write("Enter your Character= ");
+                char c=Convert.ToChar(Console.ReadLine());
+                int n=Convert.ToInt32 (c);
+                Console.WriteLine($"Result= {n}");
+                Console.ReadLine();
+            }
+            else if (op==2)
+            {
+                Console.Write("Enter your Number= ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                char c = Convert.ToChar(n);
+                Console.WriteLine($"Result= {c}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Wrong Option Selected");
+                Console.ReadLine();
+            }
+        }
+    }
+}
